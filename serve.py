18719eb9dd3e6a91c0c05d0a52f3edb087383d9a
Your Name <you@example.com>
2023-03-05 21:10:17 +0300

    kookok

--- serve.py
+++ serve.py
@@ -7,7 +7,8 @@ filepath = "testfile1GB"
 PORT = 8000
 Handler = http.server.SimpleHTTPRequestHandler
 if not os.path.exists(filepath):
-    open(filepath, "w").write("A" * 1024**3)
+    with open(filepath, "w") as f:
+        f.write("A" * 1024**3)
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at http://localhost:{PORT}")
     httpd.serve_forever()
